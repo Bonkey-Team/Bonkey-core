0a594106f5108b40d23d807b2b2b1a7e575eafc7
Bonkey Donkey <admin@bonkey.org>
2021-03-10 22:19:58 -0800

    #1 add withdraw

--- hardhat.config.js
+++ hardhat.config.js
@@ -8,19 +8,27 @@ const { projectId, mnemonic } = require('./secrets.json');
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.5.16",
-  networks: {
-     mainnet: {
-       url: `https://mainnet.infura.io/v3/${projectId}`,
-       accounts: {mnemonic: mnemonic}
-     },
-     rinkeby: {
-       url: `https://rinkeby.infura.io/v3/${projectId}`,
-       accounts: {mnemonic: mnemonic}
-     },
-     bsc_test: {
-       url: 'https://data-seed-prebsc-1-s1.binance.org:8545',
-       accounts: {mnemonic: mnemonic}
-     }
-  },
+    solidity: {
+        version: "0.5.16",
+        //settings: {
+        //    optimizer: {
+        //        enabled: true,
+        //        runs: 1000
+        //    }
+        //}
+    },
+    networks: {
+        mainnet: {
+            url: `https://mainnet.infura.io/v3/${projectId}`,
+            accounts: {mnemonic: mnemonic}
+        },
+        rinkeby: {
+            url: `https://rinkeby.infura.io/v3/${projectId}`,
+            accounts: {mnemonic: mnemonic}
+        },
+        bsc_test: {
+            url: 'https://data-seed-prebsc-1-s1.binance.org:8545',
+            accounts: {mnemonic: mnemonic}
+        }
+    },
 };
