47a7c366248e1188bc7642ceb7a4127d9ed54bfc
Bonkey Donkey <admin@bonkey.org>
2021-03-30 13:40:12 -0700

    #35 metadata and enumerable NFT

--- contracts/NFTMetadata.sol
+++ contracts/NFTMetadata.sol
@@ -5,79 +5,101 @@ import "./NFT.sol";
 import "./interfaces/IERC721Metadata.sol";
 
 contract NFTokenMetadata is
-  NFToken,
-  ERC721Metadata
+NFToken,
+    ERC721Metadata
 {
 
-  string internal nftName;
-  string internal nftSymbol;
-  mapping (uint256 => string) internal idToUri;
+    address public _manager;
+    string internal nftName;
+    string internal nftSymbol;
+    mapping (uint256 => string) internal idToUri;
 
 
-  constructor()
-  {
-    supportedInterfaces[0x5b5e139f] = true; // ERC721Metadata
-  }
-
-
-  function name()
-    external
-    override
-    view
-    returns (string memory _name)
-  {
-    _name = nftName;
-  }
-
-
-  function symbol()
-    external
-    override
-    view
-    returns (string memory _symbol)
-  {
-    _symbol = nftSymbol;
-  }
-
-
-  function tokenURI(
-    uint256 _tokenId
-  )
-    external
-    override
-    view
-    validNFToken(_tokenId)
-    returns (string memory)
-  {
-    return idToUri[_tokenId];
-  }
-
-
-  function _burn(
-    uint256 _tokenId
-  )
-    internal
-    override
-    virtual
-  {
-    super._burn(_tokenId);
-
-    if (bytes(idToUri[_tokenId]).length != 0)
+    constructor()
     {
-      delete idToUri[_tokenId];
+        supportedInterfaces[0x5b5e139f] = true; // ERC721Metadata
     }
-  }
 
 
-  function _setTokenUri(
-    uint256 _tokenId,
-    string memory _uri
-  )
-    internal
+    function set_manager(address manager) external override
+    {
+       _manager = manager; 
+    }
+
+
+    function add_product(uint256 product_id,
+                         string  calldata product_uri) external override
+    {
+        require(msg.sender == _manager, 'only manager can create product');
+        _mint(msg.sender, product_id);
+        _setTokenUri(product_id, product_uri);
+    }
+
+
+    function redeem_product(uint256 product_id) external override {
+        require(msg.sender == _manager, 'only manager can create product');
+        _burn(product_id);
+    }
+
+
+    function name()
+        external
+        override
+        view
+        returns (string memory _name)
+    {
+        _name = nftName;
+    }
+
+
+    function symbol()
+        external
+        override
+        view
+        returns (string memory _symbol)
+    {
+        _symbol = nftSymbol;
+    }
+
+
+    function tokenURI(
+            uint256 _tokenId
+            )
+        external
+        override
+        view
+        validNFToken(_tokenId)
+        returns (string memory)
+    {
+        return idToUri[_tokenId];
+    }
+
+
+    function _burn(
+            uint256 _tokenId
+            )
+        internal
+        override
+        virtual
+    {
+        super._burn(_tokenId);
+
+        if (bytes(idToUri[_tokenId]).length != 0)
+        {
+            delete idToUri[_tokenId];
+        }
+    }
+
+
+    function _setTokenUri(
+            uint256 _tokenId,
+            string memory _uri
+            )
+        internal
     validNFToken(_tokenId)
-  {
-    idToUri[_tokenId] = _uri;
-  }
+    {
+        idToUri[_tokenId] = _uri;
+    }
 
 }
 
--- contracts/interfaces/IERC721Metadata.sol
+++ contracts/interfaces/IERC721Metadata.sol
@@ -3,18 +3,25 @@ pragma solidity 0.8.0;
 
 interface ERC721Metadata
 {
-  function name()
-    external
-    view
-    returns (string memory _name);
+    function name()
+        external
+        view
+        returns (string memory _name);
 
-  function symbol()
-    external
-    view
-    returns (string memory _symbol);
+    function symbol()
+        external
+        view
+        returns (string memory _symbol);
 
-  function tokenURI(uint256 _tokenId)
-    external
-    view
-    returns (string memory);
+    function tokenURI(uint256 _tokenId)
+        external
+        view
+        returns (string memory);
+
+    function set_manager(address manager) external;
+
+    function add_product(uint256 product_id,
+                         string  calldata product_uri) external;
+
+    function redeem_product(uint256 product_id) external;
 }
