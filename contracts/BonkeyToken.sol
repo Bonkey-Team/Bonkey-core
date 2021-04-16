a4f177404e6788fe5eb905b4fe5439bc843e3848
Bonkey Donkey <admin@bonkey.org>
2021-04-15 20:37:29 -0700

    #43 Create Bonkey V2 token

--- contracts/BonkeyToken.sol
+++ contracts/BonkeyToken.sol
@@ -38,7 +38,8 @@ contract BonkeyToken is BEP20 {
         }
     } 
 
-    function mint(address _to, uint256 _amount) public onlyOwner {
+    // comply with BEP20 standard
+    function mintTo(address _to, uint256 _amount) public onlyOwner {
         _mint(_to, _amount);
         _moveDelegates(address(0), _delegates[_to], _amount);
     }
